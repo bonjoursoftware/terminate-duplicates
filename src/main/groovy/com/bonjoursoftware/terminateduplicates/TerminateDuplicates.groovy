2adfc4ebef71661525909d11e020237dbbb10b3c
achatain <achatain@users.noreply.github.com>
2019-06-05 23:34:38 +0100

    Update class header license

--- src/main/groovy/com/bonjoursoftware/terminateduplicates/TerminateDuplicates.groovy
+++ src/main/groovy/com/bonjoursoftware/terminateduplicates/TerminateDuplicates.groovy
@@ -1,8 +1,11 @@
 /**
  * Terminate Duplicates - Groovy Spock Gradle CLI app that finds and deletes duplicate files
+ *
  * https://github.com/bonjoursoftware/terminate-duplicates
  *
- * Copyright (C) 2019 Bonjour Software Limited | https://bonjoursoftware.com/
+ * Copyright (C) 2019 Bonjour Software Limited
+ *
+ * https://bonjoursoftware.com/
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -15,7 +18,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see
+ * along with this program. If not, see
  * https://github.com/bonjoursoftware/terminate-duplicates/blob/master/LICENSE
  */
 package com.bonjoursoftware.terminateduplicates
